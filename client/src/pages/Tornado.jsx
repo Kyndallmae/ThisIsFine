b748221d958fbdd16720e40d3c3556cb9196eeaf
Kyndallmae <kyndallmae@gmail.com>
2023-11-13 20:44:15 -0600

    modified tornadoes page

--- client/src/pages/Tornado.jsx
+++ client/src/pages/Tornado.jsx
@@ -17,10 +17,23 @@ const Tornado = (props) => {
           <div className="card-body text-dark">Tornadoes can be deadly, so watch out! Follow this link to learn more or check your local weather down below.</div>
         </div>
       </div>
-      <div className="col-6 col-lg-10">
-       <div className="card disply-flex mr-5">
+      <div className="col-12 col-lg-10">
+        <div className="card disply-flex mb-4">
           <h4 className="card-header bg-dark text-white p-2">What to do in case of a tornado.</h4>
-          <div className="flex-box justify-left flex-row justify-left mb-4 text-dark">1. Take shelter</div>
+          <div className="flex-box justify-left flex-row text-dark">
+            <ol>
+              <li>Take shelter in a room with no outward facing walls.</li>
+              <li>Have a flashlight and fresh water with you.</li>
+              <li>Do not leave that spot until you are absolutly sure the storm is over.</li>
+            </ol>
+          </div>
+        </div>
+      </div>
+      <div className="col-12 col-lg-10">
+        <div className="card disply-flex mb-4">
+          <h4 className="card-header bg-dark text-white p-2">Check the Weather in Your Area.</h4>
+          <div className="flex-box justify-left flex-row text-dark">
+          </div>
         </div>
       </div>
     </main>
