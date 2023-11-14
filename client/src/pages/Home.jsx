14ed6fd8d921e62dbf8aed368bcde66d85844373
casanaa <antoniocasana@yahoo.com>
2023-11-13 18:52:04 -0600

    made two columns

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -29,6 +29,26 @@ const Home = () => {
           )}
         </div>
       </div>
+
+      <div className="flex-row justify-center">
+        <div
+          className="col-12 col-md-10 mb-3 p-3"
+          style={{ border: '1px dotted #1a1a1a' }}
+        >
+          <ThoughtForm />
+        </div>
+        <div className="col-12 col-md-8 mb-3">
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+            <ThoughtList
+              thoughts={thoughts}
+              title="Tips and Tricks"
+            />
+          )}
+        </div>
+      </div>
+
     </main>
   );
 };
