e4b594507c3ff0d32d79ee8bd58344d316440b25
Kyndallmae <kyndallmae@gmail.com>
2023-11-13 21:15:16 -0600

    modified hurricanes and header index

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,19 +1,28 @@
 import { Link } from 'react-router-dom';
 
 import Auth from '../../utils/auth';
+import thisIsFine from '../../assets/images/thisisfine.jpeg'
 
 const Header = () => {
   const logout = (event) => {
     event.preventDefault();
     Auth.logout();
   };
+
   return (
     <header className="bg-primary text-primary mb-4 py-3 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <div>
+          <img src={thisIsFine}></img>
+        </div>
+        <div>
+          <Link className="text-light" to="/"></Link>
+
           <Link className="text-primary" to="/">
-            <h1 className="m-0 ">This Is Fine</h1>
+
+            <h1 className="m-0 text-danger">This Is Fine</h1>
           </Link>
+
           <p className="m-0">An emergency preparedness guide for when things are not so fine!
                    Click on the emergency you would like to learn more about.</p>
         </div>
@@ -29,11 +38,11 @@ const Header = () => {
             </>
           ) : (
             <>
-              <Link className="btn btn-lg btn-light m-2" to="/tornado">
+              <Link className="btn btn-lg btn-light m-2 text-danger" to="/tornado">
                 Tornados
               </Link>
-              <Link className="btn btn-lg btn-light m-2" to="/hurricain">
-                Hurricains
+              <Link className="btn btn-lg btn-light m-2 text-danger" to="/hurricain">
+                Hurricanes
               </Link>
             </>
           )}
--- client/src/pages/Hurricain.jsx
+++ client/src/pages/Hurricain.jsx
@@ -6,20 +6,20 @@ import { ADD_USER } from '../utils/mutations';
 
 import Auth from '../utils/auth';
 
-  return (
+return (
     <main className="flex-row justify-center mb-4">
       <div className="col-12 col-lg-10">
         <div className="card">
-          <h4 className="card-header bg-dark text-white p-2">Tornadoes</h4>
-          <div className="card-body text-dark">Tornadoes can be deadly, so watch out! Follow this link to learn more or check your local weather down below.</div>
+          <h4 className="card-header bg-dark text-white p-2">Huricanes</h4>
+          <div className="card-body text-dark">Hurricanes occur mostly in states that border the ocean, but have been known to travel farther inland.</div>
         </div>
       </div>
       <div className="col-12 col-lg-10">
         <div className="card disply-flex mb-4">
-          <h4 className="card-header bg-dark text-white p-2">What to do in case of a tornado.</h4>
+          <h4 className="card-header bg-dark text-white p-2">What to do in case of a hurricane.</h4>
           <div className="flex-box justify-left flex-row text-dark">
             <ol>
-              <li>Take shelter in a room with no outward facing walls.</li>
+              <li>Take shelter in a room with no outward facing walls or evacute if instructed to do so.</li>
               <li>Have a flashlight and fresh water with you.</li>
               <li>Do not leave that spot until you are absolutly sure the storm is over.</li>
             </ol>
@@ -37,4 +37,5 @@ import Auth from '../utils/auth';
   );
 ;
 
+Hurricane();
 export default Hurricain;
